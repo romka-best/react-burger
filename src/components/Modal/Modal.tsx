5b23104bff777cbed6e15865c493b22688c1efa2
romka-best <romka.best2016@yandex.ru>
2022-04-13 02:42:03 +0300

    fix: Улучшить код после код-ревью 12.04.22

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,28 +1,61 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-
 import {CloseIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
-import OrderDetails from '../OrderDetails/OrderDetails';
-import IngredientDetails from '../IngredientDetails/IngredientDetails';
+import ModalOverlay from '../ModalOverlay/ModalOverlay';
 
 import modalStyles from './Modal.module.css';
 
-const Modal = ({onClose, type, children}) => {
-  return (
-    <div className={modalStyles.root}>
-      <div className={modalStyles.closeButton} onClick={onClose}>
-        <CloseIcon type='primary' />
-      </div>
-      {type === 'orderDetails' && <OrderDetails/>}
-      {type === 'ingredientDetails' && <IngredientDetails ingredientDetails={children}/>}
-    </div>
+const Modal = ({children, onClose}) => {
+  const modalRoot = document.getElementById('modals') as HTMLElement;
+  const [show, setShow] = React.useState(false);
+
+  React.useEffect(() => {
+    setShow(true);
+    document.addEventListener('keydown', handleEscClose);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscClose);
+    }
+  }, []);
+
+  const handleEscClose = (evt) => {
+    if (evt.code === 'Escape') {
+      closeModal();
+    }
+  }
+
+  const handleCloseModal = (evt) => {
+    if (evt.target.classList.contains(modalStyles.root_opened)) {
+      closeModal();
+    }
+    evt.stopPropagation();
+  }
+
+  const closeModal = () => {
+    setShow(false);
+  }
+
+  return ReactDOM.createPortal((
+      <ModalOverlay show={show}>
+        <div className={`${modalStyles.root} ${show && modalStyles.root_opened}`} onClick={handleCloseModal}
+             onTransitionEnd={!show ? onClose : null}>
+          <div className={modalStyles.wrapper}>
+            <div className={modalStyles.closeButton} onClick={closeModal}>
+              <CloseIcon type='primary'/>
+            </div>
+            {children}
+          </div>
+        </div>
+      </ModalOverlay>
+    ),
+    modalRoot
   );
 }
 
 Modal.propTypes = {
-  type: PropTypes.string,
-  children: PropTypes.object,
+  children: PropTypes.element.isRequired,
   onClose: PropTypes.func.isRequired,
 }
 
--- src/components/ModalOverlay/ModalOverlay.tsx
+++ src/components/ModalOverlay/ModalOverlay.tsx
@@ -1,51 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-
-import Modal from '../Modal/Modal';
 
 import modalOverlayStyles from './ModalOverlay.module.css';
 
-const ModalOverlay = ({type, data, onClose}) => {
-  const modalRoot = document.getElementById('modals') as HTMLElement;
-  const [show, setShow] = React.useState(false);
-
-  React.useEffect(() => {
-    setShow(true);
-    document.addEventListener('keydown', handleEscClose);
-  }, []);
-
-  const handleEscClose = (evt) => {
-    if (evt.code === 'Escape') {
-      closeModal();
-    }
-  }
-
-  const handleCloseModal = (evt) => {
-    if (evt.target.classList.contains(modalOverlayStyles.root_opened)) {
-      closeModal();
-    }
-    evt.stopPropagation();
-  }
-
-  const closeModal = () => {
-    setShow(false);
-    document.removeEventListener('keydown', handleEscClose);
-  }
-
-  return ReactDOM.createPortal((
-      <div className={`${modalOverlayStyles.root} ${show && modalOverlayStyles.root_opened}`} onClick={handleCloseModal}
-           onTransitionEnd={!show ? onClose : null}>
-        <Modal onClose={closeModal} type={type}>{data}</Modal>
+const ModalOverlay = ({children, show}) => {
+  return (
+      <div className={`${modalOverlayStyles.root} ${show && modalOverlayStyles.root_opened}`}>
+        {children}
       </div>
-    ), modalRoot
   );
 }
 
 ModalOverlay.propTypes = {
-  type: PropTypes.string,
-  data: PropTypes.object,
-  onClose: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired,
+  show: PropTypes.bool.isRequired
 }
 
 export default ModalOverlay;
